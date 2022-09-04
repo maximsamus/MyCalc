90d1b0be4e7d8d558bfda860261bfaacd32e4888
maximsamus <maximsamus1@gmail.com>
2022-09-04 19:27:47 +0300

    interface Update

---
+++ MyCalc/CalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorViewController.swift
+//  MyCalc
+//
+//  Created by Максим Самусь on 4.09.2022.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+}
+
