b9cd4f13a999f34ec130967d4801f3bb2f1f4f13
maximsamus <maximsamus1@gmail.com>
2022-09-04 13:59:33 +0300

    Initial Commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by Максим Самусь on 4.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
