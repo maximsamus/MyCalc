455e3abfe3b9a765e1af70e0a64a3ba29f4005bb
maximsamus <maximsamus1@gmail.com>
2022-09-05 14:54:43 +0300

    Logic Update

--- MyCalc/CalculatorViewController.swift
+++ MyCalc/CalculatorViewController.swift
@@ -8,27 +8,39 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
-    
+        
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        guard let number = Double(displayLabel.text ?? "") else { return }
+        
+        switch sender.currentTitle {
+        case "+/-": displayLabel.text = String(format: "%.0f", number * -1)
+        case "AC": displayLabel.text = String(0)
+        default: displayLabel.text = String(number / 100)
+        }
     }
     
     @IBAction func numberButtonPressed(_ sender: UIButton) {
         
         if let numValue = sender.currentTitle {
-            displayLabel.text = numValue
-
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
         }
     }
-    
-    
 }
+
+
 
